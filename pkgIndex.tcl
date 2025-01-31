864f5740999395e23fef4b469b1cc070cb464a60
Danilo Chang <ray2501@gmail.com>
2025-01-31 20:00:11 +0800

    Bump version to v0.4

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded imagebytes 0.3 \
-	    [list load [file join $dir libtcl9imagebytes0.3.so] [string totitle imagebytes]]
+    package ifneeded imagebytes 0.4 \
+	    [list load [file join $dir libtcl9imagebytes0.4.so] [string totitle imagebytes]]
 } else {
-    package ifneeded imagebytes 0.3 \
-	    [list load [file join $dir libimagebytes0.3.so] [string totitle imagebytes]]
+    package ifneeded imagebytes 0.4 \
+	    [list load [file join $dir libimagebytes0.4.so] [string totitle imagebytes]]
 }
